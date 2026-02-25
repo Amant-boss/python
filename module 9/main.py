e770b36891f6b2ce7cd91f26c727d7de28713751
Amant-boss <zabeliamant4@gmail.com>
2026-02-25 18:42:00 +0100

    w

--- module 9/main.py
+++ module 9/main.py
@@ -5,3 +5,15 @@ tuple_length = len((10,20,30))
 print(string_length)
 print(list_length)
 print(tuple_length)
+
+sum_of_list = sum([10, 30, 40])
+sum_of_tuple = sum((5 ,20, 34))
+
+print(sum_of_list)
+print(sum_of_tuple)
+
+max_value = max(5,10,3)
+max_float = max(2.4,5.3,3.6)
+
+print(max_value)
+print(max_float)
