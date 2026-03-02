013d5e370b382c3a6c2f08f445311bf2aa13d6e2
Amant-boss <zabeliamant4@gmail.com>
2026-03-02 19:17:17 +0100

    w

---
+++ module 10/challenge.py
@@ -0,0 +1,81 @@
+class DigitalSchool:
+    def __init__(self, name, city, state, courses):
+        self.__name = name
+        self.__city = city
+        self.__state = state
+        self.__courses = courses
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, value):
+        self.__city = value
+
+    @property
+    def state(self):
+        return self.__state
+
+    @state.setter
+    def state(self, value):
+        self.__state = value
+
+    @property
+    def courses(self):
+        return self.__courses
+
+    @courses.setter
+    def courses(self, value):
+        self.__courses = value
+
+    def show_school_info(self):
+        return {
+            "name": self.__name,
+            "city": self.__city,
+            "state": self.__state,
+            "courses": self.__courses
+        }
+
+    def organize_hackathon(self):
+        raise NotImplementedError("Subclasses must implement this method.")
+
+class DS_Prishtina(DigitalSchool):
+    def __init__(self, name, city, state, courses, student_number):
+        super().__init__(name, city, state, courses)
+        self.__student_number = student_number
+
+    @property
+    def student_number(self):
+        return self.__student_number
+
+    @student_number.setter
+    def student_number(self, value):
+        self.__student_number = value
+
+    def SCF(self):
+        print("Spring Code Fest (SCF) is being organized in DS_Prishtina!")
+
+    def organize_hackathon(self):
+        print("DS_Prishtina is organizing an exciting hackathon for developers!")
+
+ds = DS_Prishtina(
+    name="Digital School Prishtina",
+    city="Prishtina",
+    state="Kosovo",
+    courses=["Python", "Web Development", "Data Science"],
+    student_number=100
+)
+
+ds.SCF()
+ds.organize_hackathon()
+
+print(f"The number of students in DS_Prishtina is {ds.student_number}.")
