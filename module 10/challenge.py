3e3ec3524a40b1b40c216d8b016cc763c8f2d711
Amant-boss <zabeliamant4@gmail.com>
2026-03-04 19:17:00 +0100

    w

--- module 10/challenge.py
+++ module 10/challenge.py
@@ -10,72 +10,63 @@ class DigitalSchool:
         return self.__name
 
     @name.setter
-    def name(self, value):
-        self.__name = value
+    def name(self, name):
+        self.__name = name
 
     @property
     def city(self):
         return self.__city
 
     @city.setter
-    def city(self, value):
-        self.__city = value
+    def city(self, city):
+        self.__city = city
 
     @property
     def state(self):
         return self.__state
 
     @state.setter
-    def state(self, value):
-        self.__state = value
+    def state(self, state):
+        self.__state = state
 
     @property
     def courses(self):
         return self.__courses
 
     @courses.setter
-    def courses(self, value):
-        self.__courses = value
+    def courses(self, courses):
+        self.__courses = courses
 
     def show_school_info(self):
-        return {
-            "name": self.__name,
-            "city": self.__city,
-            "state": self.__state,
-            "courses": self.__courses
-        }
+        print("Name : ", self.__name)
+        print("City : ", self.__city)
+        print("State : ", self.__state)
+        print("Courses : ", self.__courses)
 
     def organize_hackathon(self):
-        raise NotImplementedError("Subclasses must implement this method.")
+        print("We are organizing a hackathon")
 
-class DS_Prishtina(DigitalSchool):
+class Ds_Prishtina(DigitalSchool):
     def __init__(self, name, city, state, courses, student_number):
         super().__init__(name, city, state, courses)
-        self.__student_number = student_number
+        self._student_number = student_number
 
     @property
     def student_number(self):
-        return self.__student_number
-
-    @student_number.setter
-    def student_number(self, value):
-        self.__student_number = value
+        return self._student_number
 
     def SCF(self):
-        print("Spring Code Fest (SCF) is being organized in DS_Prishtina!")
+        print("Spring code fest is organized in Digital School")
 
     def organize_hackathon(self):
-        print("DS_Prishtina is organizing an exciting hackathon for developers!")
+        print("Ds_prishtina is organizing a hackathon")
 
-ds = DS_Prishtina(
-    name="Digital School Prishtina",
+ds = Ds_Prishtina(
+    name="Digital School",
     city="Prishtina",
-    state="Kosovo",
-    courses=["Python", "Web Development", "Data Science"],
-    student_number=100
+    state="Kosova",
+    courses="Coding courses", # Ensure this is lowercase to match __init__
+    student_number=1724072304
 )
 
-ds.SCF()
-ds.organize_hackathon()
-
-print(f"The number of students in DS_Prishtina is {ds.student_number}.")
+print(f"The number of student in Ds_prishtina is {ds.student_number}")
