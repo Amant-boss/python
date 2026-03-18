35b43a41615367dcf10a2682dc77b84cf1a409b9
Amant-boss <zabeliamant4@gmail.com>
2026-03-18 19:14:41 +0100

    w

---
+++ module14/line_plot.py
@@ -0,0 +1,16 @@
+df = pd.read_csv("avgIQpercountry.csv")
+
+avg_iq_by_continent = df.groupby("Continent")["Average IQ"].mean()
+
+plt.figure(figsize = (10,6))
+
+avg_iq_by_continent.plot(kind = "line" , marker = "o")
+
+plt.title("Average IQ by Continent")
+
+plt.xlabel("Continent")
+plt.ylabel("Average IQ")
+
+plt.grid(axis = "both", linestyle = "--", alpha = 0.7)
+
+plt.show()
