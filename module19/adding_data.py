0c6300ea691b6b5e98c055211fc139d1fc369d0b
Amant-boss <zabeliamant4@gmail.com>
2026-04-15 19:17:52 +0200

    w

---
+++ module19/adding_data.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import streamlit as st
+import plotly_express as px
+from scipy.stats import false_discovery_control
+
+books_df = pd.read_csv("bestsellers_with_categories_2022_03_27.csv")
+
+st.title("Bestselling Books")
+st.write("Analyzes best selling books from 2009 to 2022")
+
+st.sidebar.header("Add new book data")
+with st.sidebar.form("book_form"):
+    new_name = st.text_input("Book Name")
+    new_author = st.text_input("Author ")
+    new_user_rating = st.slider("User Rating", 0.0,5.0,0.0,0.1)
+    new_reviews = st.number_input("Reviews", min_value = 0, step = 1)
+    new_price = st.number_input("Price", min_value = 0, step = 1)
+    new_year = st.number_input("Year", min_value = 2009, max_value = 2022, step = 1)
+    new_genre = st.selectbox("Genre", books_df["Genre"].unique())
+    submit_button = st.form_submit_button(label = "Add Book")
+
+if submit_button:
+    new_data = {
+        "Name": new_name,
+        "Author": new_author,
+        "User Rating": new_user_rating,
+        "Reviews": new_reviews,
+        "Price": new_price,
+        "Year": new_year,
+        "Genre": new_genre
+    }
+
+    books_df = pd.concat([pd.DataFrame(new_data, index = [0]),books_df],ignore_inde=True)
+    books_df.to_csv("bestsellers_with_categories_2022_03_27.csv", index = False)
+    st.sidebar.success("New Book Added")
+
+st.subheader("Summary Statistics")
+total_books = books_df.shape[0]
+unique_titles = books_df["Name"].nunique()
+average_rating = books_df["User Rating"].mean()
+average_price = books_df["Price"].mean()
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Total Books", total_books)
+col2.metric("Unique Titles", unique_titles)
+col3.metric("Average Rating", f"(average_rating:.2)")
+col4.metric("Average Price", f"(average_price:.2)")
+
+st.subheader("Dataset Preview")
+st.write(books_df.head())
+
+with col1:
+    st.subheader("Top 10 book Titles")
+    top_titles = books_df["Name"].value_counts().head(10)
+    st.bar_chart(top_titles)
+
+with col2:
+    st.subheader("Top 10 book Authors")
+    top_authors = books_df["Author"].value_counts().head(10)
+    st.bar_chart(top_authors)
+
+st.subheader("genre distribution")
+fig = pc.pie(books_df, names = "Genre", title = "Most liked Genre", color="Genre",
+             color_discrete_sequence = px.color.sequential.Plasma)
+st.plotly_chart(fig)
+
+st.subheader("numbber of fiction vs non fiction books")
+size = books_df.groupby(["Year", "Genre"]).size().reset_index(name = "Counts")
+fig = px.bar(size, x = "Year", y = "Counts", color = "Genre", tile = "Number of fiction vs non fiction from 2009 to 2022",
+             color_discrete_sequence=px.colors.sequential.Plasma, barmode="Group")
+st.plotly_chart(fig)
